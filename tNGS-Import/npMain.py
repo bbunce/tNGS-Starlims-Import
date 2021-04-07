c3fbf5ee377c580ad8c6f64558594a38ff8df482
bbunce <b.bunce@nhs.net>
2021-04-07 14:27:56 +0100

    added variants to inputFile.starSamples dict

--- tNGS-Import/npMain.py
+++ tNGS-Import/npMain.py
@@ -1,28 +1,47 @@
 # tNGS starlims import file format .xlxs
 # starlims sequencing load file format .txt
 
-import os, shutil
+import os
+import openpyxl
 from npInputFiles import InputFiles
-from npRegex import Regex
+from npImportWs import ImportWorksheet
+from npVariant import Variant
 
 # input file paths
 varPath = r"C:\Users\bhsbu\dev\Work\tNGS-Starlims-Import\data\2020-07-14_2004549_variants.14-07-2020_STARLiMS_import.xlsx"
-starPath = r"C:\Users\bhsbu\dev\Work\tNGS-Starlims-Import\data\PL2000003-02-01"
+starPath = r"C:\Users\bhsbu\dev\Work\tNGS-Starlims-Import\data\PL2000003-02-01.txt"
 
-tNGS = InputFiles(varPath, starPath)
+inputFiles = InputFiles(varPath, starPath)
 
 # create new directory to store all working files
 try:
-    workDir = f"{os.path.dirname(varPath)}\\{tNGS.tngsID} tNGS import files\\"
+    workDir = f"{os.path.dirname(varPath)}\\{inputFiles.tngsID} tNGS import files\\"
     os.mkdir(workDir)
 except FileExistsError:
     pass
-    # shutil.rmtree(workDir) # delete directory and contents
 
-Regex = Regex(varPath, tNGS.tngsID, workDir)
+importWs = ImportWorksheet(varPath, inputFiles.tngsID, workDir)
+
+# loop through variants in varPath and assign to sample_var dictionary
+sample_var = inputFiles.starSamples
+for i in range(2, importWs.ws_main_noRows+1):
+    sampleID = importWs.ws_main.cell(row=i, column=1).value
+    if sampleID in sample_var:
+        sample_var[sampleID].append(Variant(importWs.ws_main, i))
+
+for x in sample_var:
+    if sample_var[x][2].variantPresent == True:
+        if len(sample_var[x]) == 3:
+            print("1 variant")
+            print(x, sample_var[x][2].gene)
+        elif len(sample_var[x]) > 3:
+            print("2 variants")
+            print(x, sample_var[x][2].gene)
+            print(x, sample_var[x][3].gene)
+    else:
+        print("No variants")
+        print(x)
 
 
-# tNGS.check()
-# Regex.createCopy()
 
 
